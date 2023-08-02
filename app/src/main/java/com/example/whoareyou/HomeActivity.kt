69ef4b7264acef02c21d65ccff572be1d609d6ba
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-02 20:26:40 +0900

    setImageResource를 이용하여 랜덤이미지 뿌리기

--- app/src/main/java/com/example/whoareyou/HomeActivity.kt
+++ app/src/main/java/com/example/whoareyou/HomeActivity.kt
@@ -2,10 +2,14 @@ package com.example.whoareyou
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private val image = arrayListOf(R.drawable.ic_profile, R.drawable.ic_profile_faker, R.drawable.ic_profile_android)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -15,11 +19,14 @@ class HomeActivity : AppCompatActivity() {
         val age = findViewById<TextView>(R.id.tv_age)
         val mbti = findViewById<TextView>(R.id.tv_mbti)
         val btnFinished = findViewById<Button>(R.id.btn_finish)
+        val icProfile = findViewById<ImageView>(R.id.ic_profile)
 
         id.text = intent.getStringExtra("id").toString()
         name.text = intent.getStringExtra("name").toString()
         age.text = intent.getStringExtra("age").toString()
         mbti.text = intent.getStringExtra("mbti").toString()
+
+        icProfile.setImageResource(image.random())
 
         btnFinished.setOnClickListener {
             finish()
