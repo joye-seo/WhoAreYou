324c20bed75937603024fb68913540e51b85b4b0
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-05 23:56:50 +0900

    SignUpActivity -Pw errorMessage 띄우기

--- app/src/main/java/com/example/whoareyou/SignUpActivity.kt
+++ app/src/main/java/com/example/whoareyou/SignUpActivity.kt
@@ -2,12 +2,20 @@ package com.example.whoareyou
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+lateinit var pw: EditText
+
+lateinit var errorId: TextView
+lateinit var errorPw: TextView
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,16 +23,30 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         val id = findViewById<EditText>(R.id.edt_id)
-        val pw = findViewById<EditText>(R.id.edt_pw)
+        pw = findViewById<EditText>(R.id.edt_pw)
         val name = findViewById<EditText>(R.id.edt_name)
         val age = findViewById<EditText>(R.id.edt_age)
         val mbti = findViewById<EditText>(R.id.edt_mbti)
         val btnSignUp = findViewById<Button>(R.id.btn_sign)
 
-        val errorId = findViewById<TextView>(R.id.tv_error_id)
-        val errorPw = findViewById<TextView>(R.id.tv_error_pw)
+        errorId = findViewById(R.id.tv_error_id)
+        errorPw = findViewById(R.id.tv_error_pw)
+
+        pw.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                editTextIdError()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
 
         btnSignUp.setOnClickListener {
+
 
             if (id.text.toString().isEmpty() || pw.text.toString().isEmpty() || name.text.toString().isEmpty()) {
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
@@ -48,5 +70,14 @@ class SignUpActivity : AppCompatActivity() {
             }
         }
 
+    }
+}
+
+fun editTextIdError() {
+
+    if (pw.length() < 10) {
+        errorPw.visibility = View.VISIBLE
+    } else {
+        errorPw.visibility = View.GONE
     }
 }
